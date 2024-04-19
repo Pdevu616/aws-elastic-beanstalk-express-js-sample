7435185b2e5debfbb8a6efcbd4d31ece761308b6
Pdevu616 <pranith616@gmail.com>
2024-04-18 22:23:36 -0400

    test 03

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('{"message": "hello world"}'));
+app.get('/', (req, res) => res.send('{"I am glad I completed this lab"}'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
